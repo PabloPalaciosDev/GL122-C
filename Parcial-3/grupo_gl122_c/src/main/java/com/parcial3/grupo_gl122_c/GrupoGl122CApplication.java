b2b9fe59b1a8b4ed759542397e0316aaffee04fa
Pablo Palacios <ppalacios500@gmail.com>
2022-11-22 08:45:09 -0500

    parcial3-inicio

---
+++ Parcial-3/grupo_gl122_c/src/main/java/com/parcial3/grupo_gl122_c/GrupoGl122CApplication.java
@@ -0,0 +1,13 @@
+package com.parcial3.grupo_gl122_c;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrupoGl122CApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrupoGl122CApplication.class, args);
+	}
+
+}
---
+++ Parcial-3/grupo_gl122_c/src/test/java/com/parcial3/grupo_gl122_c/GrupoGl122CApplicationTests.java
@@ -0,0 +1,13 @@
+package com.parcial3.grupo_gl122_c;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrupoGl122CApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
